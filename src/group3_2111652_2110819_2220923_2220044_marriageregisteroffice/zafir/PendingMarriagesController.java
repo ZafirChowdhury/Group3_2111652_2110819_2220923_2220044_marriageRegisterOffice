bc4beb0b72b15bed540cae3b5879127463b97e7c
ZafirChowdhury <zafirchowdhury69@gmail.com>
2024-03-30 12:38:16 +0600

    Add

---
+++ src/group3_2111652_2110819_2220923_2220044_marriageregisteroffice/zafir/PendingMarriagesController.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package group3_2111652_2110819_2220923_2220044_marriageregisteroffice.zafir;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+
+/**
+ * FXML Controller class
+ *
+ * @author zafir
+ */
+public class PendingMarriagesController implements Initializable {
+
+    @FXML
+    private TableView<?> table;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
