bd8d83252a9006b39c1de1017504396f974a14ad
AfiyaSufi <afiya.sufi27@gmail.com>
2024-04-08 00:52:38 +0600

    dashboard Done

---
+++ src/group3_2111652_2110819_2220923_2220044_marriageregisteroffice/sufi/ViewFeedbackController.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package group3_2111652_2110819_2220923_2220044_marriageregisteroffice.sufi;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author User
+ */
+public class ViewFeedbackController implements Initializable {
+
+    @FXML
+    private Label userIDL;
+    @FXML
+    private Label userNameL;
+    @FXML
+    private Label feedbackL;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void closeWindow(MouseEvent event) {
+    }
+    
+}
